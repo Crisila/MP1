da3bd2f8e1d7c6f0dfeacc3288c4ae69bae9d971
Crisila Aban <Crisila@MacBook-Pro-Alisirc.local>
2023-02-26 11:55:25 -0800

    added lines for letters

--- script.js
+++ script.js
@@ -1,79 +1,4 @@
-// reference
-const stickman = document.getElementsByClassName("stickman");
-const wrongLetters = document.getElementsByClassName("wrong-letters");
-const playerAns = document.getElementsByClassName("player-ans");
-const result = document.getElementById("player-result");
-const playAgain = document.getElementById("play-again");
-
-
-// // letter options
-// let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-// 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-// OPTION FOR RANDOM CATEGORIES
-// // make randomized categories --> not working for me
-// // https://www.kirupa.com/html5/picking_random_item_from_array.htm
-// let categories = ['Country', 'Birds', 'Mammals', 'Body', 'Furnitures'];
-
-// let random = categories[Math.floor(Math.random() * categories.length)]
-// console.log(`The chosen category is ${random}`);
-
-
-    // Country: [
-    //     "Ethiopia",
-    //     "Guatemala",
-    //     "Madagascar",
-    //     "Solomon Island",
-    //     "Slovakia",
-    //     "Zimbabwe"
-    // ], 
-    // Birds: [
-    //     "Golden Eagle",
-    //     "European Starling",
-    //     "Red-Winged Blackbird",
-    //     "Wood Duck",
-    //     "Turkey Vulture",
-    //     "Bee Hummingbird"
-    // ], 
-    // Mammals: [
-    //     "Platypus",
-    //     "Elephant",
-    //     "Flying Squirrel",
-    //     "Humpback Whale",
-    //     "Armadillo",
-    //     "Fennec Fox"
-    // ], 
-    // Body: [
-    //     "Head",
-    //     "Hands",
-    //     "Arms",
-    //     "Elbow",
-    //     "Knees",
-    //     "Back"
-    // ], 
-    // Furnitures: [
-    //     "Chair",
-    //     "Table",
-    //     "Desk",
-    //     "Bed",
-    //     "Coffee Table",
-    //     "Cabinet"
-    // ]
-
-// API OPTION
-// // random english word API
-// $.ajax({
-//     method: 'GET',
-//     url: 'https://api.api-ninjas.com/v1/randomword',
-//     headers: { 'X-Api-Key': 'nlmxQGWnUrUfUVpADnT7FQ==ZRbOyJtZjsnbtgFr'},
-//     contentType: 'application/json',
-//     success: function(result) {
-//         console.log(result);
-//     },
-//     error: function ajaxError(jqXHR) {
-//         console.error('Error: ', jqXHR.responseText);
-//     }
-// });
+//array of words
 
 const words = [
     "Platypus",
@@ -101,26 +26,66 @@ const words = [
     "Coffee Table",
     "Cabinet"];
 
-let randomWord = words[Math.floor(Math.random() * words.length)];
-// console.log(randomWord);
 
-
-// let options = function () {
-//     letterButtons = document.getElementById('categ-buttons')
-//     alphabet = document.createElement('ul')
-
-//     for (let i = 0; i < letters.length; i++) {
-//         // const element = array[i];
-//        alphabet.id = 'selection'
-//        list = document.createElement('li')
-//         list.innerHTML = letters[i];
-//         letterButtons.appendChild(alphabet);
-//         letters.appendChild(list);
-//     }
-
-// }
-
+//choose word randomly
+const randWord = Math.floor(Math.random() * words.length);
+const chosenWord = words[randWord]; 
 const correctAns = [];
-const wrongAns = [];
+const incorrectAns = [];
+const lines = [];
 
-// display correct word
+
+
+// DOM
+let playerAns = document.getElementsByClassName("player-ans")
+let playerResult = document.getElementById("player-result")
+let wrongLetters = document.getElementsByClassName('wrong-letters')
+
+
+
+// display number of lines per letter of the word
+//issue: spaces are being counted
+//issue: double letters
+//issue: capital letters 
+
+console.log(chosenWord);
+
+let genLines = () => {
+    for (let i = 0; i < chosenWord.length; i++) {
+        lines.push('_'); 
+    }
+    return lines
+}
+
+// console.log(genLines());
+
+//get user's guess
+document.addEventListener('keypress', (event) => {
+    let key = event.key
+
+
+//display correct letters
+    if (chosenWord.indexOf(key) > -1) {
+        correctAns.push(key);
+        // replace lines to letters
+        lines[chosenWord.indexOf(key)] = key;
+        playerAns[0].innerHTML = lines.join(' ');
+        playerResult.innerHTML = correctAns;
+        // check if word matches 
+        // console.log(lines)
+        if (lines.join('') == chosenWord) {
+            alert('Winner!')
+        }
+    }
+    // display incorrect letters used
+    else {
+        incorrectAns.push(key);
+        wrongLetters[0].innerHTML = incorrectAns;
+    }
+});
+
+//to replace each letter with a lines --> genLines
+
+playerAns[0].innerHTML = genLines().join('');
+
+
